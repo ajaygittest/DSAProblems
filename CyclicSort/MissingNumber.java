e0d2965ed3f9b20901d29c92392b36915e066d2a
ajaygittest <yesurajadgl@gmail.com>
2025-04-10 21:54:26 +0530

    Missing Number in Array

---
+++ CyclicSort/MissingNumber.java
@@ -0,0 +1,37 @@
+package CyclicSort;
+
+public class MissingNumber {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int missingNumber(int[] arr) {
+		int i=0;
+		
+		while(i<arr.length) {
+			int correct=arr[i]-1;
+			
+			if(arr[i]> 0 &&	arr[i]<=arr.length&& arr[i] !=arr[correct]) {
+				swap(arr,i,correct);
+			}else {
+				i++;
+			}
+		}
+		
+		for(int index=0;index<arr.length;index++) {
+			if(arr[index] !=index+1) {
+				return index+1;
+			}
+		}
+		return arr.length+1;
+	}
+
+	private static void swap(int[] arr, int i, int correct) {
+		int temp=arr[i];
+		arr[i]=arr[correct];
+		arr[correct]=temp;
+		
+	}
+
+}
