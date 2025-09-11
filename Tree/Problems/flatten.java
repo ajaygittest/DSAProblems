df8a1736a1bef3c124fcb5535bd8bf367df5d294
ajaygittest <yesurajadgl@gmail.com>
2025-09-11 21:25:03 +0530

    Flattern using tree

---
+++ Tree/Problems/flatten.java
@@ -0,0 +1,30 @@
+package Tree;
+
+public class flatten {
+	
+	
+	public void flatten(TreeNode root) {
+		
+		if(root ==null) {
+			return;
+		}
+		
+		TreeNode current=root;
+		while(current !=null) {
+			if(current.left !=null) {
+				TreeNode temp=current.left;
+				while(temp.right !=null) {
+					temp=temp.right;
+					
+				}
+				temp.right=current.right;
+				current.right=current.left;
+				current.left=null;
+				
+			}
+			current=current.right;
+		}
+	        
+	    }
+}
+
