9d5403494cb639fabe0f4cf3e61563dd85f92370
ajaygittest <yesurajadgl@gmail.com>
2025-10-12 11:03:03 +0530

    Next permutation problem implementation

---
+++ CheatSheet/NextPermutation.java
@@ -0,0 +1,50 @@
+package Own;
+
+public class NextPermutation {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	 public void nextPermutation(int[] nums) {
+	     
+		 int i=nums.length-2;
+		 int n=nums.length;
+		 
+		 
+		 while(i>=0&&nums[i]>=nums[i+1])
+		 {
+			 i--;
+		 }
+		 
+		 if(i>=0) {
+			int j=n-1;
+			 while(j>=0 && nums[j]<=nums[i]) {
+				 j--;
+			 }
+			 swap(nums,i,j);
+		 }
+		 
+		 reverse(nums,i+1,n-1);
+		
+		 
+	    }
+
+
+	private void reverse(int[] nums, int start, int end) {
+		 while (start < end) {
+	            swap(nums, start++, end--);
+	        }
+		
+	}
+
+
+	private void swap(int[] nums, int start, int end) {
+		int temp=nums[start];
+		nums[start]=nums[end];
+		nums[end]=temp;
+		
+	}
+
+}
