c9ddf96f6bb420470dba235bf6d6412e4a3601ce
ajaygittest <yesurajadgl@gmail.com>
2026-04-09 15:15:28 +0530

    detect cycle problem

---
+++ DSA-Pattern/LinkedList/DetectCycle.java
@@ -0,0 +1,29 @@
+package DSA.DSAProblems.FastandSlowPointer;
+
+public class DetectCycle {
+	
+	public Node hasCycle(Node head) {
+		
+		Node slow=head;
+		Node fast=head;
+		
+		while(fast !=null && fast.next !=null) {
+			
+			slow=slow.next;
+			fast=fast.next.next;
+			
+			if(slow==fast) {
+				slow=head;
+				
+				while(slow !=fast) {
+					slow=slow.next;
+					fast=fast.next;
+				}
+				return slow;
+			}
+			
+		}
+		return null;
+	}
+
+}
